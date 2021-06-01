a1d49e2fa23a186039e896fbfe251c473d206795
Jean Carlos Holguin <holguinjean1@gmail.com>
2021-06-01 18:29:01 -0400

    Agregar archivos de proyecto.

---
+++ ContactsApp/Entities/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Entities {
+    public class Contact {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int GroupID { get; set; }
+        public Group Group { get; set; }
+    }
+}
---
+++ ContactsApp/Entities/ContactContactType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Entities {
+    public class ContactContactType {
+        public int ID { get; set; }
+        public IEnumerable<ContactType> ContactTypes { get; set; }
+        public IEnumerable<Contact> Contacts { get; set; }
+        public string Number { get; set; }
+    }
+}
